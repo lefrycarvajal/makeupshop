c3dd280911d02fbe5b294400197968aec6b45f51
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-08-16 03:24:03 -0400

    Update conexion_db.php

--- conexion_db.php
+++ conexion_db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","dbmakeupc");
+$conex = mysqli_connect("ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","kqhjh9zs1j00djk2","co32o62y312hqfbp","	pe3z5r7ji67v3q5e");
 
 ?>
